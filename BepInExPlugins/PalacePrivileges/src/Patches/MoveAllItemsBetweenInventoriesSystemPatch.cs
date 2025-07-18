f79e340097451d2f4bf001d7c0cd6adbb76c8c7b
Nicholas Toby <cheesasaurus@gmail.com>
2025-07-18 01:22:36 -0500

    PalacePrivs: MoveAllItemsBetweenInventories patches

---
+++ BepInExPlugins/PalacePrivileges/src/Patches/MoveAllItemsBetweenInventoriesSystemPatch.cs
@@ -0,0 +1,87 @@
+using HarmonyLib;
+using ProfuselyViolentProgression.Core.Utilities;
+using ProjectM;
+using ProjectM.CastleBuilding;
+using ProjectM.Network;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ProfuselyViolentProgression.PalacePrivileges.Patches;
+
+
+[HarmonyPatch]
+public unsafe class MoveAllItemsBetweenInventoriesSystemPatch
+{
+    private static EntityManager _entityManager = WorldUtil.Game.EntityManager;
+
+
+    [HarmonyPatch(typeof(MoveAllItemsBetweenInventoriesSystem), nameof(MoveAllItemsBetweenInventoriesSystem.OnUpdate))]
+    [HarmonyPrefix]
+    public static void MoveAllItemsBetweenInventoriesSystem_OnUpdate_Prefix(MoveAllItemsBetweenInventoriesSystem __instance)
+    {
+        if (!Core.IsInitialized)
+        {
+            return;
+        }
+
+        var query = __instance._EventQuery;
+        var entities = query.ToEntityArray(Allocator.Temp);
+        var fromCharacters = query.ToComponentDataArray<FromCharacter>(Allocator.Temp);
+        var moveAllItemsEvents = query.ToComponentDataArray<MoveAllItemsBetweenInventoriesEvent>(Allocator.Temp);
+
+        var networkIdToEntityMap = Core.NetworkIdService.NetworkIdToEntityMap();
+
+        for (var i = 0; i < entities.Length; i++)
+        {
+            ProcessMoveAllItemsBetweenInventoriesEvent(entities[i], fromCharacters[i], moveAllItemsEvents[i], ref networkIdToEntityMap);
+        }
+    }
+
+    private static void ProcessMoveAllItemsBetweenInventoriesEvent(
+        Entity eventEntity,
+        FromCharacter fromCharacter,
+        MoveAllItemsBetweenInventoriesEvent moveAllItemsEvent,
+        ref NetworkIdLookupMap networkIdToEntityMap
+    )
+    {
+        if (!networkIdToEntityMap.TryGetValue(moveAllItemsEvent.FromInventory, out var fromInventory))
+        {
+            return;
+        }
+
+        if (!networkIdToEntityMap.TryGetValue(moveAllItemsEvent.ToInventory, out var toInventory))
+        {
+            return;
+        }
+
+        var isFromCastleHeartInventory = _entityManager.TryGetComponentData<CastleHeart>(fromInventory, out var fromCastleHeart);
+        if (isFromCastleHeartInventory)
+        {
+            HandleMoveAllItems_FromCastleHeartInventory(
+                eventEntity: eventEntity,
+                fromCharacter: fromCharacter,
+                fromInventory: fromInventory,
+                toInventory: toInventory,
+                castleHeartEntity: fromInventory,
+                castleHeart: fromCastleHeart,
+                moveItemEvent: moveAllItemsEvent
+            );
+            return;
+        }
+    }
+
+    private static void HandleMoveAllItems_FromCastleHeartInventory(
+        Entity eventEntity,
+        FromCharacter fromCharacter,
+        Entity fromInventory,
+        Entity toInventory,
+        Entity castleHeartEntity,
+        CastleHeart castleHeart,
+        MoveAllItemsBetweenInventoriesEvent moveItemEvent
+    )
+    {
+        // todo: implement
+        LogUtil.LogDebug("moving all items FROM castle heart.");
+    }
+
+}
---
+++ BepInExPlugins/PalacePrivileges/src/Patches/MoveAllItemsBetweenInventoriesV2SystemPatch.cs
@@ -0,0 +1,87 @@
+using HarmonyLib;
+using ProfuselyViolentProgression.Core.Utilities;
+using ProjectM;
+using ProjectM.CastleBuilding;
+using ProjectM.Network;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace ProfuselyViolentProgression.PalacePrivileges.Patches;
+
+
+[HarmonyPatch]
+public unsafe class MoveAllItemsBetweenInventoriesV2SystemPatch
+{
+    private static EntityManager _entityManager = WorldUtil.Game.EntityManager;
+
+
+    [HarmonyPatch(typeof(MoveAllItemsBetweenInventoriesV2System), nameof(MoveAllItemsBetweenInventoriesV2System.OnUpdate))]
+    [HarmonyPrefix]
+    public static void MoveAllItemsBetweenInventoriesV2System_OnUpdate_Prefix(MoveAllItemsBetweenInventoriesV2System __instance)
+    {
+        if (!Core.IsInitialized)
+        {
+            return;
+        }
+
+        var query = __instance._EventQuery;
+        var entities = query.ToEntityArray(Allocator.Temp);
+        var fromCharacters = query.ToComponentDataArray<FromCharacter>(Allocator.Temp);
+        var moveAllItemsEvents = query.ToComponentDataArray<MoveAllItemsBetweenInventoriesEventV2>(Allocator.Temp);
+
+        var networkIdToEntityMap = Core.NetworkIdService.NetworkIdToEntityMap();
+
+        for (var i = 0; i < entities.Length; i++)
+        {
+            ProcessMoveAllItemsBetweenInventoriesV2Event(entities[i], fromCharacters[i], moveAllItemsEvents[i], ref networkIdToEntityMap);
+        }
+    }
+
+    private static void ProcessMoveAllItemsBetweenInventoriesV2Event(
+        Entity eventEntity,
+        FromCharacter fromCharacter,
+        MoveAllItemsBetweenInventoriesEventV2 moveAllItemsEvent,
+        ref NetworkIdLookupMap networkIdToEntityMap
+    )
+    {
+        if (!networkIdToEntityMap.TryGetValue(moveAllItemsEvent.FromInventory, out var fromInventory))
+        {
+            return;
+        }
+
+        if (!networkIdToEntityMap.TryGetValue(moveAllItemsEvent.ToInventory, out var toInventory))
+        {
+            return;
+        }
+
+        var isFromCastleHeartInventory = _entityManager.TryGetComponentData<CastleHeart>(fromInventory, out var fromCastleHeart);
+        if (isFromCastleHeartInventory)
+        {
+            HandleMoveAllItems_FromCastleHeartInventory(
+                eventEntity: eventEntity,
+                fromCharacter: fromCharacter,
+                fromInventory: fromInventory,
+                toInventory: toInventory,
+                castleHeartEntity: fromInventory,
+                castleHeart: fromCastleHeart,
+                moveItemEvent: moveAllItemsEvent
+            );
+            return;
+        }
+    }
+
+    private static void HandleMoveAllItems_FromCastleHeartInventory(
+        Entity eventEntity,
+        FromCharacter fromCharacter,
+        Entity fromInventory,
+        Entity toInventory,
+        Entity castleHeartEntity,
+        CastleHeart castleHeart,
+        MoveAllItemsBetweenInventoriesEventV2 moveItemEvent
+    )
+    {
+        // todo: implement
+        LogUtil.LogDebug("moving all items FROM castle heart. V2");
+    }
+
+}
